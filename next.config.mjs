594b2bfa4cf7adcbb5e48d7d3cfef3ddc70c3e59
Simon Beltran <simonbeltran8134@gmail.com>
2026-03-25 10:55:55 +0200

    SEO: Ajout de la redirection www vers non-www pour EUHM

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,21 @@ const nextConfig = {
   images: {
     remotePatterns: [],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.euhm.fr',
+          },
+        ],
+        destination: 'https://euhm.fr/:path*',
+        permanent: true, // C'est la fameuse redirection 301 (SEO Gold)
+      },
+    ]
+  },
 };
 
 export default nextConfig;
